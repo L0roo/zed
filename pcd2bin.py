5a5e25fb426330542e7cdc96d3f8f61c23f8a62c
L0roo <laurent.hoffmann729@gmail.com>
2024-05-15 19:17:53 +0200

    rotation

--- pcd2bin.py
+++ pcd2bin.py
@@ -10,11 +10,11 @@ points = np.zeros([pcd_data.width, 6], dtype=np.float32)
 points[:, 0] = pcd_data.pc_data['x'].copy()
 points[:, 1] = pcd_data.pc_data['y'].copy()
 points[:, 2] = pcd_data.pc_data['z'].copy()
-points[:,:3] = points[:,:3] *2.0# scaling makes the visualization work somehow, also has a big influence on bboxes
+points[:,:3] = points[:,:3] *5.0# scaling makes the visualization work somehow, also has a big influence on bboxes
 #multiply by 2 or so for viewer data, divide by 1000 if from python script
 
 
-#random color, reduce dim above from 6 to 2
+#random color, reduce dim above from 6 to 3
 #points_color = np.random.randint(size= (pcd_data.width, 3),low=0, high=255)
 #points_color = points_color.astype(np.uint8)
 #points_color = points_color + 2570
@@ -25,7 +25,7 @@ points[:,:3] = points[:,:3] *2.0# scaling makes the visualization work somehow, 
 
 
 #subsample
-perc = 0.01
+perc = 0.01 # higher percentage, might need more scaling
 mask = np.random.choice([True, False], size=len(points), p=[perc, 1-perc])
 
 
@@ -38,6 +38,19 @@ for i in range(pcd_data.width):
 
 
 points = points[mask]
+
+#rotate points
+deg_x = np.deg2rad(45)
+deg_y = np.deg2rad(80)
+deg_z = np.deg2rad(20)
+rot_mat_x = np.array([[1, 0, 0],[0, np.cos(deg_x), -np.sin(deg_x)],[0, np.sin(deg_x), np.cos(deg_x)]])
+rot_mat_y = np.array([[np.cos(deg_y), 0, np.sin(deg_y)],[0, 1, 0],[-np.sin(deg_y), 0, np.cos(deg_y)]])
+rot_mat_z = np.array([[np.cos(deg_z), -np.sin(deg_z), 0],[np.sin(deg_z), np.cos(deg_z), 0],[0, 0, 1]])
+for i in range(len(points)):
+    points[i,:3]= np.matmul(rot_mat_x,points[i,:3])
+    points[i,:3]= np.matmul(rot_mat_y,points[i,:3])
+    points[i,:3]= np.matmul(rot_mat_z,points[i,:3])
+
 
 
 points = points.astype(np.float32)
