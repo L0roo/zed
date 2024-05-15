2fa210e110f16479b9f04aa9e697330004a37918
L0roo <laurent.hoffmann729@gmail.com>
2024-05-15 18:48:45 +0200

    color

--- pcd2bin.py
+++ pcd2bin.py
@@ -1,11 +1,12 @@
 import numpy as np
 from pypcd import pypcd
+import struct
 
-input_file = 'test_gui2.pcd'
+input_file = 'test_gui.pcd'
 output_path = "data/bin/"+input_file[:-3] + 'bin'
 
 pcd_data = pypcd.PointCloud.from_path("data/pcd/"+input_file)
-points = np.zeros([pcd_data.width, 3], dtype=np.float32)
+points = np.zeros([pcd_data.width, 6], dtype=np.float32)
 points[:, 0] = pcd_data.pc_data['x'].copy()
 points[:, 1] = pcd_data.pc_data['y'].copy()
 points[:, 2] = pcd_data.pc_data['z'].copy()
@@ -13,12 +14,12 @@ points[:,:3] = points[:,:3] *2.0# scaling makes the visualization work somehow, 
 #multiply by 2 or so for viewer data, divide by 1000 if from python script
 
 
-
-points_color = np.random.randint(size= (pcd_data.width, 3),low=0, high=255)
+#random color, reduce dim above from 6 to 2
+#points_color = np.random.randint(size= (pcd_data.width, 3),low=0, high=255)
 #points_color = points_color.astype(np.uint8)
 #points_color = points_color + 2570
 
-points = np.hstack((points,points_color))
+#points = np.hstack((points,points_color))
 
 
 
@@ -26,7 +27,18 @@ points = np.hstack((points,points_color))
 #subsample
 perc = 0.01
 mask = np.random.choice([True, False], size=len(points), p=[perc, 1-perc])
+
+
+
+for i in range(pcd_data.width):
+    if mask[i]:
+        char_array = struct.unpack('BBBB', pcd_data.pc_data['rgb'][i])
+        points[i,3:] = char_array[:3]
+        #print("(R,G,B,A) = {}".format(char_array))
+
+
 points = points[mask]
+
 
 points = points.astype(np.float32)
 print(points)
--- test_color.py
+++ test_color.py
@@ -5,10 +5,15 @@ from pypcd import pypcd
 input_file = 'test1.pcd'
 
 pcd_data = pypcd.PointCloud.from_path("data/pcd/"+input_file)
+points = np.zeros([pcd_data.width, 3], dtype=np.float32)
+
+perc = 0.01
+mask = np.random.choice([True, False], size=len(points), p=[perc, 1-perc])
+points = points[mask]
 
 
-#err, point_cloud_value = point_cloud.get_data(x,y)    # Get your point cloud value at pixel (x,y)
-#packed = struck.pack('f', point_cloud_value[3])
+
 for i in range(pcd_data.width):
-    char_array = struct.unpack('BBBB', pcd_data.pc_data['rgb'][i])
-    print("(R,G,B,A) = {}".format(char_array))
+    if mask[i]:
+        char_array = struct.unpack('BBBB', pcd_data.pc_data['rgb'][i])
+        print("(R,G,B,A) = {}".format(char_array)+" i:"+str(i))
