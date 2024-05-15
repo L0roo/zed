61ae30b07859dccbea725f0a37b6a10e2979c773
L0roo <laurent.hoffmann729@gmail.com>
2024-05-15 18:32:41 +0200

    update

--- pcd2bin.py
+++ pcd2bin.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pypcd import pypcd
 
-input_file = 'test1.pcd'
+input_file = 'test_gui2.pcd'
 output_path = "data/bin/"+input_file[:-3] + 'bin'
 
 pcd_data = pypcd.PointCloud.from_path("data/pcd/"+input_file)
@@ -9,10 +9,8 @@ points = np.zeros([pcd_data.width, 3], dtype=np.float32)
 points[:, 0] = pcd_data.pc_data['x'].copy()
 points[:, 1] = pcd_data.pc_data['y'].copy()
 points[:, 2] = pcd_data.pc_data['z'].copy()
-#points[:, 3] = pcd_data.pc_data['intensity'].copy().astype(np.float32)
-#mean = np.mean(points[:,:3], axis=0)
-#std_dev = np.std(points[:,:3], axis=0)
-points[:,:3] = points[:,:3] / 1000 # do if .pcd via python not ZED depth viewer (scale)
+points[:,:3] = points[:,:3] *2.0# scaling makes the visualization work somehow, also has a big influence on bboxes
+#multiply by 2 or so for viewer data, divide by 1000 if from python script
 
 
 
@@ -22,8 +20,7 @@ points_color = np.random.randint(size= (pcd_data.width, 3),low=0, high=255)
 
 points = np.hstack((points,points_color))
 
-# Normalize each column
-#points[:,:3] = (points[:,:3] - mean) / std_dev
+
 
 
 #subsample
--- test_color.py
+++ test_color.py
@@ -9,5 +9,6 @@ pcd_data = pypcd.PointCloud.from_path("data/pcd/"+input_file)
 
 #err, point_cloud_value = point_cloud.get_data(x,y)    # Get your point cloud value at pixel (x,y)
 #packed = struck.pack('f', point_cloud_value[3])
-char_array = struct.unpack('BBBB', pcd_data.pc_data['rgb'][0])
-print("(R,G,B,A) = {}".format(char_array))
+for i in range(pcd_data.width):
+    char_array = struct.unpack('BBBB', pcd_data.pc_data['rgb'][i])
+    print("(R,G,B,A) = {}".format(char_array))
