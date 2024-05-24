543ee2a5a6df9339be84060a487cb25ab0b1a2fc
L0roo <laurent.hoffmann729@gmail.com>
2024-05-24 15:57:24 +0200

    removed copy of points for speed

--- demo_svo.py
+++ demo_svo.py
@@ -47,50 +47,16 @@ deg_z = np.deg2rad(0)
 rot_mat_x = np.array([[1, 0, 0], [0, np.cos(deg_x), -np.sin(deg_x)], [0, np.sin(deg_x), np.cos(deg_x)]])
 rot_mat_y = np.array([[np.cos(deg_y), 0, np.sin(deg_y)], [0, 1, 0], [-np.sin(deg_y), 0, np.cos(deg_y)]])
 rot_mat_z = np.array([[np.cos(deg_z), -np.sin(deg_z), 0], [np.sin(deg_z), np.cos(deg_z), 0], [0, 0, 1]])
+np.random.seed(427)
 
 size_list = []
 time_list = []
+inferencer_time_list = []
+filter_time_list = []
+depth_filter_time_list = []
+sub_filter_time_list = []
+color_filter_time_list = []
 
-def pcd2bin(pcd_data):
-    num_points = np.shape(pcd_data)[0]
-    points = np.zeros([num_points, 6], dtype=np.float32)
-    points[: , :3] = pcd_data[:,:3]
-
-
-    mask2 = points[:,2] < max_depth*unit
-    points = points[mask2]
-
-    #subsample
-    perc = ppc / len(points)
-    mask = np.random.choice([True, False], size=len(points), p=[perc, 1-perc])
-    points = points[mask]
-
-    points[:, :3] = points[:, :3] * (scale / unit)
-    if read_color:
-        for i in range(len(points)):
-            char_array = struct.unpack('BBBB', pcd_data[i,3])
-            points[i,3:] = char_array[:3]
-    else:
-        points_color = np.random.randint(size= (len(points), 3),low=0, high=255)
-        points[:,3:] = points_color
-
-
-
-
-
-
-    #rotate points
-    for i in range(len(points)):
-        points[i,:3]= np.matmul(rot_mat_x,points[i,:3])
-        points[i,:3]= np.matmul(rot_mat_y,points[i,:3])
-        points[i,:3]= np.matmul(rot_mat_z,points[i,:3])
-
-
-
-    points = points.astype(np.float32)
-    print(np.shape(points))
-    size_list.append(np.shape(points)[0])
-    return points
 
 
 
@@ -158,6 +124,61 @@ def parse_args():
     return init_args, call_args
 
 
+
+
+def pcd2bin(point_cloud):
+    points = point_cloud.get_data()[:, :, :4]
+    points = points.reshape((-1, 4))
+
+    st_depth = time.time()
+    mask2 = points[:,2] < max_depth*unit
+    points = points[mask2]
+    et_depth = time.time()
+
+    #subsample
+    st_sub = time.time()
+    perc = ppc / len(points)
+    mask = np.random.choice([True, False], size=len(points), p=[perc, 1-perc])
+    points = points[mask]
+    et_sub = time.time()
+
+    st_color = time.time()
+    points[:, :3] = points[:, :3] * (scale / unit)
+    if read_color:
+        col_list = []
+        for i in range(len(points)):
+            col_list.append(struct.unpack('BBBB', points[i,3])[:3])
+        points = np.hstack((points[:,:3],np.array(col_list)))
+
+    else:
+        points_color = np.random.randint(size= (len(points), 3),low=0, high=255)
+        points = np.hstack((points[:,:3],points_color))
+    et_color = time.time()
+
+
+
+
+
+
+    #rotate points
+    #for i in range(len(points)):
+    #    points[i,:3]= np.matmul(rot_mat_x,points[i,:3])
+    #    points[i,:3]= np.matmul(rot_mat_y,points[i,:3])
+    #    points[i,:3]= np.matmul(rot_mat_z,points[i,:3])
+
+
+
+    points = points.astype(np.float32)
+    print(np.shape(points))
+    size_list.append(np.shape(points)[0])
+
+    depth_filter_time_list.append(et_depth-st_depth)
+    sub_filter_time_list.append(et_sub-st_sub)
+    color_filter_time_list.append(et_color-st_color)
+
+    return points
+
+
 def main():
     init_args, call_args = parse_args()
 
@@ -173,19 +194,26 @@ def main():
 
     point_cloud = sl.Mat()
     i = 0
+    frame_counter = 0
     start_time = time.time()
     while i < 10000:
         if zed.grab() == sl.ERROR_CODE.SUCCESS:
             if i % downsampling == 0:
                 st = time.time()
+                frame_counter +=1
                 zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
-                point_cloud_np = point_cloud.get_data()[:, :, :4]
-                point_cloud_np = point_cloud_np.reshape((-1, 4))
-                points = pcd2bin(point_cloud_np)
-                call_args['inputs'] = dict(points=points)
+                st_filter = time.time()
+                call_args['inputs'] = dict(points=pcd2bin(point_cloud))
+                et_filter = time.time()
                 inferencer.num_visualized_imgs = i  # can not generate output files else
+                st_inferencer = time.time()
                 inferencer(**call_args)
-                print("showing frame " + str(int(i/downsampling+1)))
+                et_inferencer = time.time()
+
+                inferencer_time_list.append(et_inferencer-st_inferencer)
+                filter_time_list.append(et_filter-st_filter)
+
+                print("showing frame " + str(frame_counter))
                 if call_args['out_dir'] != '' and not (call_args['no_save_vis']
                                                        and call_args['no_save_pred']):
                     print_log(
@@ -199,10 +227,15 @@ def main():
         elif zed.grab() == sl.ERROR_CODE.END_OF_SVOFILE_REACHED:
             end_time = time.time()
             time_dif = end_time - start_time
-            time_pf = round(time_dif / (int(i / downsampling) + 1), 4)
-            print("Average time per frame: " + str(time_pf) + " s")
-            print("Average time per frame (time on used frames only): " + str(round(np.mean(time_list),4)) + " s")
-            print("Average FPS: "+str(round(1/time_pf,4)))
+            time_pf = round((time_dif / frame_counter), 4)*1000
+            print("Average time per frame total: " + str(time_pf) + " ms")
+            print("Average time per frame (time on used frames only): " + str(round(np.mean(time_list),4)*1000) + " ms")
+            print("Average time per frame (inference): " + str(round(np.mean(inferencer_time_list),4)*1000) + " ms")
+            print("Average time per frame (transform pc): " + str(round(np.mean(filter_time_list),4)*1000) + " ms")
+            print("Average time per frame (depth filter): " + str(round(np.mean(depth_filter_time_list),4)*1000) + " ms")
+            print("Average time per frame (subsample): " + str(round(np.mean(sub_filter_time_list),4)*1000) + " ms")
+            print("Average time per frame (color): " + str(round(np.mean(color_filter_time_list),4)*1000) + " ms")
+            print("Average FPS: "+str(round(1000/time_pf,4)))
             break
     zed.close()
     print("Average points per cloud: " + str(round(np.mean(size_list), 0)))
