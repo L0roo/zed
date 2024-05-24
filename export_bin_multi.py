300fbefa47289b2830b630f23ebc0130d167b434
L0roo <laurent.hoffmann729@gmail.com>
2024-05-24 11:40:29 +0200

    new and improved

--- export_bin_multi.py
+++ export_bin_multi.py
@@ -9,13 +9,14 @@ import struct
 
 # Set SVO path for playback
 input_path = "data/HD1080_SN34783283_15-12-38.svo"
-output_base = "data/bin/multi4"
-downsampling = 50 # each x frame is used
+output_base = "data/bin/multi5"
+downsampling = 5 # each x frame is used
 
 max_depth = 2.5 # in m
-scale = 3 # scale up a bit bc of white screen on visualisation, big mystery
-perc = 0.02 # percentage of points to keep,  higher percentage, might need higher scaling (is applied before max_depth reduction for speed)
+scale = 3 # scale up a bit gives higher pred scores
+perc = 0.02 # percentage of points to keep,  higher percentage, might need higher scaling (is applied before max_depth reduction for speed) obsolete
 #perc of 0.02 gives approx 30000 points
+ppc = 30000 #points per cloud, makes perc obsolete
 unit = 1000 # 1000 for mm, 1 for m, via .py gives in mm, gui gives m
 read_color = False
 
@@ -31,41 +32,34 @@ rot_mat_z = np.array([[np.cos(deg_z), -np.sin(deg_z), 0], [np.sin(deg_z), np.cos
 
 size_list = []
 
-zed = sl.Camera()
 
 def pcd2bin(pcd_data, output_path):
     num_points = np.shape(pcd_data)[0]
     points = np.zeros([num_points, 6], dtype=np.float32)
     points[: , :3] = pcd_data
-    points[:, :3] = points[:, :3] * (scale / unit)
 
 
+    mask2 = points[:,2] < max_depth*unit
+    points = points[mask2]
 
     #subsample
+    perc = ppc / len(points)
     mask = np.random.choice([True, False], size=len(points), p=[perc, 1-perc])
+    points = points[mask]
 
+    points[:, :3] = points[:, :3] * (scale / unit)
     if read_color:
-        #read color doesnt work yet
-        for i in range(num_points):
-            if mask[i]:
-                pass
-                #char_array = struct.unpack('BBBB', pcd_data.pc_data['rgb'][i])
-                #points[i,3:] = char_array[:3]
-                #print("(R,G,B,A) = {}".format(char_array))
+        pass
     else:
-        points_color = np.random.randint(size= (num_points, 3),low=0, high=255)
+        points_color = np.random.randint(size= (len(points), 3),low=0, high=255)
         points[:,3:] = points_color
 
 
-    points = points[mask]
 
 
-    mask2 = points[:,2] < max_depth*scale
-    points = points[mask2]
+
 
     #rotate points
-
-
     for i in range(len(points)):
         points[i,:3]= np.matmul(rot_mat_x,points[i,:3])
         points[i,:3]= np.matmul(rot_mat_y,points[i,:3])
@@ -78,6 +72,7 @@ def pcd2bin(pcd_data, output_path):
     size_list.append(np.shape(points)[0])
     with open(output_path, 'wb') as f:
         f.write(points.tobytes())
+
 
 
 
@@ -100,6 +95,7 @@ err = zed.open(init_parameters)
 
 point_cloud = sl.Mat()
 i=0
+st = time.time()
 while i<10000:
   if zed.grab() == sl.ERROR_CODE.SUCCESS:
     if i % downsampling == 0:
@@ -115,5 +111,10 @@ while i<10000:
     i+=1
   elif zed.grab() == sl.ERROR_CODE.END_OF_SVOFILE_REACHED:
     print("SVO end has been reached. "+str(int(i/downsampling)+1) +" pcd files generated")
+    et = time.time()
+    time_dif = et - st
+    time_pf = round(time_dif / (int(i/downsampling)+1), 4)
+    print("Time per file: " + str(time_pf) + " s")
     break
 zed.close()
+print("Average points per cloud: "+ str(round(np.mean(size_list),0)))
